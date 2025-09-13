4ac0fa3d3a90d3431a83765a710d075f11227644
faharetana-rkt <rakotomaharitra@gmail.com>
2025-09-13 09:21:04 +0300

    finished dynamic rendering of product pages

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,11 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+// get parameters from the URL
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
