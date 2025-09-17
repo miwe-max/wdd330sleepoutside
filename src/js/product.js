dbadda105327b7d82955ee4c4e188a78fc0557b4
miwe-max <miwe@byupathway.edu>
2025-09-17 02:24:40 +0100

    Resolved merge conflicts

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,32 @@
-import { getParam, renderNumberOfItemsBackpack } from "./utils.mjs";
+// imports (don’t import the same name twice)
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
-const product = new ProductDetails(productId, dataSource);
-product.init();
 
-// function to render tne superscript number of items in backpack
-renderNumberOfItemsBackpack(document.querySelector("#cart-numbers"), "so-cart");
+// ALWAYS treat the cart as an array
+function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart");
 
-// // add to cart button event handler
-// async function addToCartHandler(e) {
-//   const product = await dataSource.findProductById(e.target.dataset.id);
-//   addProductToCart(product);
-// }
+  // Normalize: if nothing there, or something wrong (object/string), start fresh as []
+  if (!Array.isArray(cart)) cart = [];
 
-// // add listener to Add to Cart button
-// document
-//   .getElementById("addToCart")
-//   .addEventListener("click", addToCartHandler);
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+}
+
+// add to cart button event handler
+async function addToCartHandler(e) {
+  // Use currentTarget to avoid issues if inner elements are clicked
+  const id = e.currentTarget?.dataset?.id || e.target?.dataset?.id;
+  if (!id) return; // optionally show a user message
+
+  const product = await dataSource.findProductById(id);
+  addProductToCart(product);
+}
+
+// Make sure the element exists on this page before adding the listener
+const btn = document.getElementById("addToCart");
+if (btn) {
+  btn.addEventListener("click", addToCartHandler);
+}
